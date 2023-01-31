3e6b3d222a0b4ef41e5788faa15d5b7260f278a4
Mark Yasutake <mark.yasutake@gmail.com>
2023-01-30 19:49:26 -0800

    POKER_SIM-1: Define Deck class
    Define Card.dealt property

--- cards.py
+++ cards.py
@@ -5,6 +5,7 @@ class Card:
         self._verify_suit(value=suit)
         self._rank = rank
         self._suit = suit
+        self._dealt = False
         return
 
     @property
@@ -14,6 +15,15 @@ class Card:
     @property
     def suit(self) -> str:
         return self._suit.lower()
+
+    @property
+    def dealt(self) -> bool:
+        return self._dealt
+
+    @dealt.setter
+    def dealt(self, value) -> None:
+        self._dealt = value
+        return
 
     @staticmethod
     def _verify_rank(value: str) -> None:
@@ -25,4 +35,52 @@ class Card:
     def _verify_suit(value: str) -> None:
         if value.lower() not in ['c', 'd', 'h', 's']:
             raise ValueError(f"Invalid card suit '{value}' given.")
+        return
+
+
+class Deck:
+
+    def __init__(self) -> None:
+        self._cards = []
+        self.shuffle()
+        return
+
+    # Properties
+
+    @property
+    def list_of_all_cards(self) -> list[Card]:
+        return self._cards
+
+    @property
+    def list_of_all_cards_dealt(self) -> list[Card]:
+        card_list = []
+        for i_card in self.list_of_all_cards:
+            if i_card.dealt:
+                card_list.append(i_card)
+        return card_list
+
+    @property
+    def list_of_all_cards_not_dealt(self) -> list[Card]:
+        card_list = []
+        for i_card in self.list_of_all_cards:
+            if not i_card.dealt:
+                card_list.append(i_card)
+        return card_list
+
+    @property
+    def number_of_cards_dealt(self) -> int:
+        return len(self.list_of_all_cards_dealt)
+
+    @property
+    def number_of_cards_not_dealt(self) -> int:
+        return len(self.list_of_all_cards_not_dealt)
+
+    # Actions
+
+    def shuffle(self) -> None:
+        self._cards = []
+        for i_rank in ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']:
+            for i_suit in ['c', 'd', 'h', 's']:
+                i_card = Card(rank=i_rank, suit=i_suit)
+                self._cards.append(i_card)
         return
