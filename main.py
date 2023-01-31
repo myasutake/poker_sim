b6ec5544204a55b580e9ba3032e4253c11984fb0
Mark Yasutake <mark.yasutake@gmail.com>
2023-01-30 20:21:51 -0800

    POKER_SIM-1: Define main.py - deals a random starting hand

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import cards
+
+
+def main():
+    deck = cards.Deck()
+    hand = deck.deal_cards(number_of_cards=2)
+    print(f'Starting hand: {hand}')
+    return
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
