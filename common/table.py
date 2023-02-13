f96ee6c72b8e2ef0feb898f612b8e1e248a15000
Mark Yasutake <mark.yasutake@gmail.com>
2023-02-12 22:27:46 -0800

    POKER_SIM-2: Define Table and Seat classes

---
+++ common/table.py
@@ -0,0 +1,35 @@
+class Table:
+
+    def __init__(self) -> None:
+        self._seats = []
+        self._populate_seats()
+        return
+
+    def _populate_seats(self) -> None:
+        seat_numbers = range(1, 10)
+        seat_names = ['UTG', 'UTG+1', 'UTG+2', 'LJ', 'HJ', 'CO', 'BTN', 'SB', 'BB']
+        for (i_seat_number, i_seat_name) in zip(seat_numbers, seat_names):
+            self._seats.append(Seat(number=i_seat_number, name=i_seat_name))
+        return
+
+    def __str__(self) -> str:
+        s = ''
+        for i_seat in self._seats:
+            s += f'Seat {i_seat.number} - {i_seat.name}\n'
+        return s
+
+
+class Seat:
+
+    def __init__(self, number: int, name: str) -> None:
+        self._number = number
+        self._name = name
+        return
+
+    @property
+    def number(self) -> int:
+        return self._number
+
+    @property
+    def name(self) -> str:
+        return self._name
