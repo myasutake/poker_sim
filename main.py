717bd4cba1037b5a70b0df297a0a92eeae7e7aca
Mark Yasutake <mark.yasutake@gmail.com>
2023-06-06 19:52:55 -0700

    POKER_SIM-3: Refactor/remove main func

--- main.py
+++ main.py
@@ -6,17 +6,12 @@
 from common import cards, table
 
 
-def main():
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
     t = table.Table()
     t.assign_hero_role_to_random_empty_seat()
     hero_seat = t.get_hero_seat()
     t.deal(number_of_cards=2, seat_number=hero_seat.number)
     print(t)
-    return
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
