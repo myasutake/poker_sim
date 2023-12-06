d63518e4c92eef87318f1fdfd7418c2d31b9a308
Mark Yasutake <mark.yasutake@gmail.com>
2023-12-05 19:58:12 -0800

    Refactor - change unit test import to remove ambiguity

--- test/test_states.py
+++ test/test_states.py
@@ -1,20 +1,20 @@
 import pytest
 
-from common import table
+import common.table
 
 
 @pytest.fixture(scope='function')
-def t() -> table.Table():
-    return table.Table()
+def table() -> common.table.Table():
+    return common.table.Table()
 
 
 class TestInit:
 
     @staticmethod
-    def test_init_state_is_init(t):
-        assert type(t.state) == table.Init
+    def test_init_state_is_init(table):
+        assert type(table.state) == common.table.Init
 
     @staticmethod
-    def test_init_auto_transitions_to_preflop(t):
-        t.run()
-        assert type(t.state) == table.PreFlop
+    def test_init_auto_transitions_to_preflop(table):
+        table.run()
+        assert type(table.state) == common.table.PreFlop
