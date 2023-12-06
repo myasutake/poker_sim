3167b0f71ff8113dc60a14d98d5a1522baec2a65
Mark Yasutake <mark.yasutake@gmail.com>
2023-12-05 20:48:39 -0800

    Add unit tests for PreFlop transitions
    * Hero - change hand
    * Hero - change seat
    * Villain - change seat

--- test/test_states.py
+++ test/test_states.py
@@ -8,6 +8,13 @@ def table_init() -> common.table.Table():
     return common.table.Table()
 
 
+@pytest.fixture(scope='function')
+def table_preflop(table_init) -> common.table.Table():
+    table = table_init
+    table.run()
+    return table
+
+
 class TestInit:
 
     @staticmethod
@@ -17,5 +24,33 @@ class TestInit:
     @staticmethod
     def test_init_auto_transitions_to_preflop(table_init):
         table = table_init
+        table.run()
+        assert type(table.state) == common.table.PreFlop
+
+
+class TestPreFlop:
+
+    @staticmethod
+    def test_hero_change_hand_transitions_to_preflop(monkeypatch, table_preflop):
+        # Monkeypatch the input(), simulate user intput "1":
+        monkeypatch.setattr('builtins.input', lambda _: "1")
+
+        table = table_preflop
+        table.run()
+        assert type(table.state) == common.table.PreFlop
+
+    @staticmethod
+    def test_hero_change_seat_transitions_to_preflop(monkeypatch, table_preflop):
+        monkeypatch.setattr('builtins.input', lambda _: "2")
+
+        table = table_preflop
+        table.run()
+        assert type(table.state) == common.table.PreFlop
+
+    @staticmethod
+    def test_villain_change_seat_transitions_to_preflop(monkeypatch, table_preflop):
+        monkeypatch.setattr('builtins.input', lambda _: "3")
+
+        table = table_preflop
         table.run()
         assert type(table.state) == common.table.PreFlop
