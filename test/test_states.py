54b1550cb92f842b72687d7ae652986233e0ce85
Mark Yasutake <mark.yasutake@gmail.com>
2023-12-05 20:00:39 -0800

    Refactor - rename unit test setup func

--- test/test_states.py
+++ test/test_states.py
@@ -4,17 +4,18 @@ import common.table
 
 
 @pytest.fixture(scope='function')
-def table() -> common.table.Table():
+def table_init() -> common.table.Table():
     return common.table.Table()
 
 
 class TestInit:
 
     @staticmethod
-    def test_init_state_is_init(table):
-        assert type(table.state) == common.table.Init
+    def test_init_state_is_init(table_init):
+        assert type(table_init.state) == common.table.Init
 
     @staticmethod
-    def test_init_auto_transitions_to_preflop(table):
+    def test_init_auto_transitions_to_preflop(table_init):
+        table = table_init
         table.run()
         assert type(table.state) == common.table.PreFlop
