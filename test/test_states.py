0c2d71a7d367affaed965b2ef8330ee02a686dec
Mark Yasutake <mark.yasutake@gmail.com>
2023-12-07 15:35:37 -0800

    Add unit tests to verify deck state after starting over

--- test/test_states.py
+++ test/test_states.py
@@ -16,6 +16,19 @@ def table_preflop(table_init) -> common.table.Table():
 
 
 @pytest.fixture(scope='function')
+def table_preflop_after_start_over(monkeypatch, table_preflop) -> common.table.Table():
+    table = table_preflop
+
+    monkeypatch.setattr('builtins.input', lambda _: "S")
+    table.run()
+    # table is now in Init state
+    table.run()
+    # table is now in PreFlop state
+
+    return table
+
+
+@pytest.fixture(scope='function')
 def table_preflop_after_hero_hand_change(monkeypatch, table_preflop) -> common.table.Table():
     table = table_preflop
 
@@ -38,6 +51,13 @@ class TestInit:
         assert type(table.state) == common.table.PreFlop
 
 
+class TestPreFlopAfterStartOver:
+
+    @staticmethod
+    def test_deck_stub_has_50_cards(table_preflop_after_start_over):
+        assert table_preflop_after_start_over.deck.number_of_cards_not_dealt == 50
+
+
 class TestPreFlop:
 
     # Table properties
@@ -47,6 +67,14 @@ class TestPreFlop:
         assert table_preflop.deck.number_of_cards_not_dealt == 50
 
     # State transitions
+
+    @staticmethod
+    def test_start_over_transitions_to_init(monkeypatch, table_preflop):
+        monkeypatch.setattr('builtins.input', lambda _: "S")
+
+        table = table_preflop
+        table.run()
+        assert type(table.state) == common.table.Init
 
     @staticmethod
     def test_hero_change_hand_transitions_to_preflop(monkeypatch, table_preflop):
